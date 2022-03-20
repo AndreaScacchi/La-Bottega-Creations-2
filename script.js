c2960cbc8c2270832b7e30b9a5c3bf0b70c04c3e
AndreaScacchi <andreascacchi10@gmail.com>
2022-03-20 17:51:24 +0100

    refactoring

--- script.js
+++ script.js
@@ -79,31 +79,7 @@ document.querySelector(".nav-links").addEventListener("click", function (e) {
   }
 });
 
-/*document.querySelector(".footer-links").addEventListener("click", function (e) {
-    e.preventDefault();
-
-    // Matching strategy
-    if (e.target.classList.contains("footer-link")) {
-        const id = e.target.getAttribute("href");
-        document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-    }
-});*/
-
-/*const footerLinks = document.querySelector(".footer-links");
-
-function scrollFooter() {
-    for(let item in footerLinks) {
-        let myNav = document.querySelector(".footer-link");
-        myNav.addEventListener("click", function() {
-            item.scrollIntoView({ behavior: "smooth" });
-        })
-    }
-}
-scrollFooter();*/
-
-document
-  .querySelector(".sidebar-links")
-  .addEventListener("click", function (e) {
+document.querySelector(".sidebar-links").addEventListener("click", function (e) {
     e.preventDefault();
 
     // Matching strategy
@@ -113,54 +89,6 @@ document
       sidebar.classList.remove("show-sidebar");
     }
   });
-
-// Reveal sections
-/*const allSections = document.querySelectorAll(".section");
-
-const revealSection = function (entries, observer) {
-    const [entry] = entries;
-
-    if (!entry.isIntersecting) return;
-
-    entry.target.classList.remove("section-hidden");
-    observer.unobserve(entry.target);
-};
-
-const sectionObserver = new IntersectionObserver(revealSection, {
-    root: null,
-    threshold: 0.15,
-});
-
-allSections.forEach(function (section) {
-    sectionObserver.observe(section);
-    section.classList.add("section-hidden");
-});*/
-
-// Loading images
-/*const imgTargets = document.querySelectorAll("img[data-src]");
-
-const loadImg = function (entries, observer) {
-    const [entry] = entries;
-
-    if (!entry.isIntersecting) return;
-
-    // Replace src with data-src
-    entry.target.src = entry.target.dataset.src;
-
-    entry.target.addEventListener("load", function () {
-        entry.target.classList.remove("lazy-img");
-    });
-
-    observer.unobserve(entry.target);
-};
-
-const imgObserver = new IntersectionObserver(loadImg, {
-    root: null,
-    threshold: 0,
-    rootMargin: "200px",
-});
-
-imgTargets.forEach((img) => imgObserver.observe(img));*/
 
 // When user clicks on the button, scroll to the top of the document
 const btnTop = document.querySelector(".btn-top");
