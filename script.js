342404d9e6649e4e9b529bcbfde9660b1607b9ed
AndreaScacchi <andreascacchi10@gmail.com>
2022-04-03 21:51:06 +0200

    new commits

--- script.js
+++ script.js
@@ -1,156 +1,44 @@
-"use strict";
+const sections = document.querySelectorAll(".section");
+const sectBtns = document.querySelectorAll(".controlls");
+const sectBtn = document.querySelectorAll(".control");
+const allSections = document.querySelector(".main-content");
 
-const navbar = document.querySelector("#nav");
-const navBtn = document.querySelector("#nav-btn");
-const closeBtn = document.querySelector("#close-btn");
-const sidebar = document.querySelector("#sidebar");
-const date = document.querySelector("#date");
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.getElementById("section-1");
-const nav = document.querySelector(".nav");
-
-// Set date
-date.innerHTML = new Date().getFullYear();
-
-// Add sticky navbar
-window.addEventListener("scroll", function () {
-  if (window.pageYOffset > 80) {
-    navbar.classList.add("navbar-fixed");
-    navBtn.style.color = "rgb(211, 19, 115)";
-  } else {
-    navbar.classList.remove("navbar-fixed");
-    navBtn.style.color = "black";
-  }
-});
-
-// Show sidebar
-navBtn.addEventListener("click", function () {
-  sidebar.classList.add("show-sidebar");
-});
-
-closeBtn.addEventListener("click", function () {
-  sidebar.classList.remove("show-sidebar");
-});
-
-//Menu fade animation
-const handleHover = function (e) {
-  if (e.target.classList.contains("nav-link")) {
-    const link = e.target;
-    const siblings = link.closest(".nav").querySelectorAll(".nav-link");
-    const logo = link.closest(".nav").querySelector("img");
-
-    siblings.forEach((el) => {
-      if (el !== link) el.style.opacity = this;
-    });
-    logo.style.opacity = this;
-  }
-};
-
-// Passing "argument" into handler
-nav.addEventListener("mouseover", handleHover.bind(0.5));
-nav.addEventListener("mouseout", handleHover.bind(1));
-
-// Button scrolling
-btnScrollTo.addEventListener("click", function (e) {
-  const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
-
-  console.log(e.target.getBoundingClientRect());
-
-  console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
-
-  console.log(
-    "height/width viewport",
-    document.documentElement.clientHeight,
-    document.documentElement.clientWidth
-  );
-
-  section1.scrollIntoView({ behavior: "smooth" });
-});
-
-// Page navigation
-document.querySelector(".nav-links").addEventListener("click", function (e) {
-  e.preventDefault();
-
-  // Matching strategy
-  if (e.target.classList.contains("nav-link")) {
-    const id = e.target.getAttribute("href");
-    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-  }
-});
-
-document.querySelector(".sidebar-links").addEventListener("click", function (e) {
-    e.preventDefault();
-
-    // Matching strategy
-    if (e.target.classList.contains("sidebar-link")) {
-      const id = e.target.getAttribute("href");
-      document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-      sidebar.classList.remove("show-sidebar");
+function PageTransitions() {
+    //Button click active class
+    for (let i = 0; i < sectBtn.length; i++) {
+        sectBtn[i].addEventListener("click", function () {
+            let currentBtn = document.querySelectorAll(".active-btn");
+            currentBtn[0].className = currentBtn[0].className.replace(
+                "active-btn",
+                ""
+            );
+            this.className += " active-btn";
+        });
     }
-  });
 
-// When user clicks on the button, scroll to the top of the document
-const btnTop = document.querySelector(".btn-top");
+    //Sctions Active
+    allSections.addEventListener("click", (e) => {
+        const id = e.target.dataset.id;
+        if (id) {
+            //resmove selected from the other btns
+            sectBtns.forEach((btn) => {
+                btn.classList.remove("active");
+            });
+            e.target.classList.add("active");
 
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 850 ||
-    document.documentElement.scrollTop > 850
-  ) {
-    btnTop.style.display = "block";
-    btnTop.innerHTML = "back to top";
-    btnTop.style.textTransform = "capitalize";
-    btnTop.style.cursor = "pointer";
-  } else {
-    btnTop.style.display = "none";
-  }
+            //hide other sections
+            sections.forEach((section) => {
+                section.classList.remove("active");
+            });
+
+            const element = document.getElementById(id);
+            element.classList.add("active");
+        }
+    });
 }
 
-window.onscroll = function () {
-  scrollFunction();
-};
+PageTransitions();
 
-const buttonTop = function () {
-  btnTop.addEventListener("click", () => {
-    window.scroll({ behavior: "smooth", top: 0 });
-  });
-};
-buttonTop();
-
-const panels = document.querySelectorAll('.panel');
-panels.forEach(panel => panel.addEventListener("click", () => {
-  removeActiveClasses();
-  panel.classList.add('active');
-}));
-
-function removeActiveClasses() {
-  panels.forEach(panel => panel.classList.remove('active'));
-}
-
-// Animate the H1 element in the header
-var textWrapper = document.querySelector(".ml12");
-textWrapper.innerHTML = textWrapper.textContent.replace(
-  /\S/g,
-  "<span class='letter'>$&</span>"
-);
-
-anime
-  .timeline({ loop: true })
-  .add({
-    targets: ".ml12 .letter",
-    translateX: [40, 0],
-    translateZ: 0,
-    opacity: [0, 1],
-    easing: "easeOutExpo",
-    duration: 1200,
-    delay: (el, i) => 500 + 30 * i,
-  })
-  .add({
-    targets: ".ml12 .letter",
-    translateX: [0, -30],
-    opacity: [1, 0],
-    easing: "easeInExpo",
-    duration: 1100,
-    delay: (el, i) => 100 + 30 * i,
-  });
+// Set year date
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
